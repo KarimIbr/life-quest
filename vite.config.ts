d96dd22c5b2a3417953b8594aad6806cd25e178a
KarimIbr <karimkhaledibrahim2003@gmail.com>
2025-03-28 15:54:15 +0100

    Fix module loading and MIME type issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: './',
   server: {
     port: 3000,
     host: true,
@@ -16,6 +16,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       },
     },
   },
